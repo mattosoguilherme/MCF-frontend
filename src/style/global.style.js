e46c51224a413b9bdbd4d90778f5a769f43262ad
Guilherme Mattoso <guilhermemktfran@gmail.com>
2022-11-07 18:33:15 -0300

    ..

---
+++ src/style/global.style.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Bebas Neue', cursive;
+  }
+`;
+
+export default GlobalStyle;
