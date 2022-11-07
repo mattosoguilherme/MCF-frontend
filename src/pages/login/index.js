31fe0dbcc612bac8733819847f25a4fef9c1a627
Guilherme Mattoso <guilhermemktfran@gmail.com>
2022-11-07 20:27:59 -0300

    ..

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,8 +1,59 @@
+import styled from "styled-components";
+
+const H1s = styled.h1`
+  font-family: "Reenie Beanie", cursive;
+  text-align: center;
+`;
+
+const ContainerS = styled.main`
+  --bs-gutter-x: 1.5rem;
+  --bs-gutter-y: 0;
+  width: 100%;
+  height: 100vh;
+  padding-right: calc(var(--bs-gutter-x) * 0.5);
+  padding-left: calc(var(--bs-gutter-x) * 0.5);
+  margin-right: auto;
+  margin-left: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const FormLogin = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content:center;
+  width: 30rem;
+
+  align-self: center;
+  background-color: black;
+  padding:2rem 0.7rem 2rem 0.7rem;
+  border-radius:2rem;
+  
+  input{
+    margin:0 0 0.7rem 0;
+  }
+  button{
+   width:8rem;
+   align-self:center;
+
+  }
+`;
+
 const Login = () => {
-    return (
-        <>
-        </>
-    )
-}
+  return (
+    <>
+      <ContainerS>
+        <H1s> Ministério Casa da Família </H1s>
+
+        <FormLogin>
+          <input />
+          <input />
+          <button> Entrar </button>
+        </FormLogin>
+      </ContainerS>
+    </>
+  );
+};
 
 export default Login;
--- src/style/global.style.js
+++ src/style/global.style.js
@@ -5,7 +5,7 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    font-family: 'Bebas Neue', cursive;
+    font-family: 'Poiret One', cursive;
   }
 `;
 
